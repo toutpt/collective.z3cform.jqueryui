4408c5627e890b1a81f2236e54e4d910a33f65f1
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-12-04 18:36:50 +0100

    get datepicker from collective.z3cform.datepicker

--- collective/z3cform/jqueryui/__init__.py
+++ collective/z3cform/jqueryui/__init__.py
@@ -3,9 +3,6 @@ MessageFactory = MessageFactory('collective.z3cform.jqueryui')
 
 from collective.z3cform.jqueryui.autocomplete.widget import AutocompleteFieldWidget
 from collective.z3cform.jqueryui.autocomplete.widget import AutocompleteMultiFieldWidget
-from collective.z3cform.jqueryui.datetime.widget_date import DateWidget
-from collective.z3cform.jqueryui.datetime.widget_date import DateFieldWidget
-from collective.z3cform.jqueryui.datetime.widget_datetime import DatetimeWidget
-from collective.z3cform.jqueryui.datetime.widget_datetime import DatetimeFieldWidget
-from collective.z3cform.jqueryui.datetime.widget_monthyear import MonthYearWidget
-from collective.z3cform.jqueryui.datetime.widget_monthyear import MonthYearFieldWidget
+from collective.z3cform.jqueryui.datetpicker.widget import DatePickerWidget, DateTimePickerWidget
+from collective.z3cform.jqueryui.datetpicker.widget import DatePickerFieldWidget, DateTimePickerFieldWidget
+
